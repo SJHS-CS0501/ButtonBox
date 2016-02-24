fcfcaf8e2a569c2f273f504c141d85374e763cef
jprotivnak <jprotivnak@me.com>
2016-02-24 08:46:17 -0500

    Added Many Things
    Basic Concept  with buttons and also some formatting for the position of
    the frame.
    Also began action listener, but finding files for sounds.

---
+++ src/SoundTest.java
@@ -0,0 +1,85 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.swing.*;
+import java.io.File;
+
+/**
+ * @author SJHSStudent
+ *
+ */
+public class SoundTest extends JFrame implements ActionListener {
+
+	private static final long serialVersionUID = 1;
+	private JFrame frame;
+	private JPanel buttonPanel;
+	private JButton button;
+
+	public SoundTest() {
+		super("SoundTest");
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(2, 3));
+
+		button = new JButton("Cat's Meow");
+		button.setActionCommand("catMeow");
+		button.addActionListener(this);
+		buttonPanel.add(button);
+
+		button = new JButton("Isabelle's Yell");
+		button.setActionCommand("yell");
+		button.addActionListener(this);
+		buttonPanel.add(button);
+
+		button = new JButton("Mr. Woytek's Boom");
+		button.setActionCommand("boom");
+		button.addActionListener(this);
+		buttonPanel.add(button);
+
+		button = new JButton("Damn Daniel");
+		button.setActionCommand("daniel");
+		button.addActionListener(this);
+		buttonPanel.add(button);
+
+		button = new JButton("What are those????");
+		button.setActionCommand("whatAreThose");
+		button.addActionListener(this);
+		buttonPanel.add(button);
+
+		button = new JButton("*Facsimile Noise*");
+		button.setActionCommand("facsimile");
+		button.addActionListener(this);
+		buttonPanel.add(button);
+
+		add(buttonPanel, BorderLayout.CENTER);
+		
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width/3-this.getSize().width/2, dim.height/3-this.getSize().height/2);
+		setResizable(false);
+		setSize(getPreferredSize());
+		pack();
+		setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		JButton button = (JButton) e.getSource();
+		switch (button.getActionCommand()) {
+			case "catMeow":
+				AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File().getAbsoluteFile());
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new SoundTest();
+
+	}
+
+}

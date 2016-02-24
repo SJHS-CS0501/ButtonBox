1dfe21f2ad79a9b4798f5c2f476b627c3b48a74c
rysmith18 <rtsmith1101@outlook.com>
2016-02-24 08:15:50 -0500

    Starting ButtonBox program
    Created project and class ButtonBox

---
+++ src/ButtonBox.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author SJHSStudent
+ *
+ */
+public class ButtonBox {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

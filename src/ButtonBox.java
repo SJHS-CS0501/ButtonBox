b1c1973a28237f1da8a324737ebf48a5f61722d9
rysmith18 <rtsmith1101@outlook.com>
2016-02-25 09:38:25 -0500

    Window appears, just need sounds to play

--- src/ButtonBox.java
+++ src/ButtonBox.java
@@ -1,36 +1,45 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.applet.*;
+import java.net.*;
 
 /**
  * 
  */
 
 /**
- * @author SJHSStudent
+ * @author Ryan Smith
  *
  */
 public class ButtonBox extends JFrame implements ActionListener{
 	
+	//declaring JLabels, JPanels and GridLayout
 	private static final long serialVersionUID = 1;
 	static JLabel myLabel;
 	private JPanel mainPanel;
 	private JPanel buttonPanel;
-	static JButton playSound;
 	static JButton quitButton;
-	static GridBagLayout layout;
+	static GridLayout layout;
 	
+	/**
+	 * Constructor
+	 */
 	public ButtonBox() {
-		super("Button Box");
+		super("Button Box");//Called because this class extends another class
 		JButton button;
 		JLabel label;
 		myLabel = new JLabel("Press a button to play a sound");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 		
+		//instantiating JPanels mainPanel and buttonPanel
 		mainPanel = new JPanel();
 		buttonPanel = new JPanel();
 		
+		buttonPanel.setLayout(new GridLayout(1, 3)); //setting the layout to 1 x 3
+		
+		//instantiating JButttons, setting the action commands, adding the listener, and adding the JButton to the JPanel
 		button = new JButton("Play sound 1");
 		button.setActionCommand("one");
 		button.addActionListener(this);
@@ -55,8 +64,25 @@ public class ButtonBox extends JFrame implements ActionListener{
 		button.setActionCommand("five");
 		button.addActionListener(this);
 		buttonPanel.add(button);
+		
+		add(buttonPanel, BorderLayout.SOUTH);
+		
+		label = new JLabel("Press a button to play a sound: ");
+		mainPanel.add(label);
+		
+		myLabel = new JLabel();
+		mainPanel.add(myLabel);
+		
+		add(mainPanel, BorderLayout.CENTER);
+		
+		setSize(getPreferredSize());
+		pack();
+		setVisible(true);
 	}
 	
+	/**
+	 * This method is the listener that plays the sound when the JButton is pressed.
+	 */
 	public void actionPerformed(ActionEvent e) {
 		JButton button = (JButton)e.getSource();
 		switch(button.getActionCommand()) {
@@ -64,15 +90,19 @@ public class ButtonBox extends JFrame implements ActionListener{
 			myLabel.setText("Playing sound 1...");
 			break;
 		case "two":
+			myLabel.setText("Playing sound 2...");
 			break;
 		case "three":
+			myLabel.setText("Playing sound 3...");
 			break;
 		case "four":
+			myLabel.setText("Playing sound 4...");
 			break;
 		case "five":
+			myLabel.setText("Playing sound 5...");
 			break;
 		default:
-			JOptionPane.showMessageDialog(this, "Unknown button pressed");
+			JOptionPane.showMessageDialog(this, "Unknown button pressed");//Will never happen.
 		}
 	}
 
